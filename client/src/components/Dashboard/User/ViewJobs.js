4856616d4ce942b4faf23c600187a7bbd6a2b696
Chukwurado <chukwurado.umeaka98@gmail.com>
2020-01-10 20:10:49 +0100

    Delete pages folder and fix bug in view jobs

--- client/src/components/Dashboard/User/ViewJobs.js
+++ client/src/components/Dashboard/User/ViewJobs.js
@@ -5,147 +5,160 @@ import googleLogo from "../../../../../client/src/google.jpg";
 import VerticalModal from "./VerticalModal";
 import FilterButtons from "./FilterButtons";
 
+import { setAuthToken } from "../../../store/actions/auth";
+
 const ViewJobs = props => {
-  const [jobs, setJobs] = useState([]);
-  const [modalShow, setModalShow] = useState(false);
-  const [jobIndex, setJobIndex] = useState();
-  const [filteredJobs, setfilteredJobs] = useState([]);
-  const [typeDropdownOpen, typeSetDropdownOpen] = useState(false);
-  const [locationDropdownOpen, locationSetDropdownOpen] = useState(false);
-  const [roleDropdownOpen, roleSetDropdownOpen] = useState(false);
-  const typeToggle = () => typeSetDropdownOpen(prevState => !prevState);
-  const locationToggle = () => locationSetDropdownOpen(prevState => !prevState);
-  const roleToggle = () => roleSetDropdownOpen(prevState => !prevState);
+    const [jobs, setJobs] = useState([]);
+    const [modalShow, setModalShow] = useState(false);
+    const [jobIndex, setJobIndex] = useState();
+    const [filteredJobs, setfilteredJobs] = useState([]);
+    const [typeDropdownOpen, typeSetDropdownOpen] = useState(false);
+    const [locationDropdownOpen, locationSetDropdownOpen] = useState(false);
+    const [roleDropdownOpen, roleSetDropdownOpen] = useState(false);
+    const typeToggle = () => typeSetDropdownOpen(prevState => !prevState);
+    const locationToggle = () =>
+        locationSetDropdownOpen(prevState => !prevState);
+    const roleToggle = () => roleSetDropdownOpen(prevState => !prevState);
 
-  useEffect(() => {
-    getJobs();
-  }, []);
+    useEffect(() => {
+        setAuthToken(localStorage.token);
+        getJobs();
+    }, []);
 
-  const getJobs = async () => {
-    try {
-      const res = await axios.get("api/jobs");
-      const jobs = res.data;
+    const getJobs = async () => {
+        try {
+            const res = await axios.get("api/jobs");
+            const jobs = res.data;
 
-      setfilteredJobs(jobs);
-      setJobs(jobs);
-      console.log(jobs);
-    } catch (error) {
-      console.log("error fetching jobs--", error);
-    }
-  };
-  console.log(jobs);
-
-  const clearFilter = () => {
-    setfilteredJobs(jobs);
-  };
-
-  const filter = (type, value) => {
-    console.log("TYPE", type);
-    console.log("VALZZ", value);
-
-    const filtered = [];
-    clearFilter();
-
-    if (type === "Type") {
-      jobs.map(job => {
-        // console.log("#2--", job);
-        if (job.typeofposition === value) {
-          filtered.push(job);
+            setfilteredJobs(jobs);
+            setJobs(jobs);
+            console.log(jobs);
+        } catch (error) {
+            console.log("error fetching jobs--", error);
         }
-      });
-    }
+    };
+    console.log(jobs);
 
-    setfilteredJobs(filtered);
-  };
+    const clearFilter = () => {
+        setfilteredJobs(jobs);
+    };
 
-  const showJob = index => {
-    setJobIndex(index);
-    setModalShow(true);
-  };
+    const filter = (type, value) => {
+        console.log("TYPE", type);
+        console.log("VALZZ", value);
 
-  return (
-    <div className={classes.ViewJobs}>
-      <VerticalModal
-        job={jobs[jobIndex]}
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
-      <div style={{ flexDirection: "row" }}>
-        <FilterButtons
-          filter={filter}
-          dropdownOpen={typeDropdownOpen}
-          toggle={() => typeToggle()}
-          filterType="Type"
-          filterItems={["Internship", "Full Time", "Contractor"]}
-        />
-        <FilterButtons
-          filter={filter}
-          dropdownOpen={locationDropdownOpen}
-          toggle={() => locationToggle()}
-          filterType="Location"
-          filterItems={["New York", "Los Angeles", "San Francisco"]}
-        />
-        <FilterButtons
-          filter={filter}
-          dropdownOpen={roleDropdownOpen}
-          toggle={() => roleToggle()}
-          filterType="Primary Role"
-          filterItems={[
-            "Software Engineer",
-            "Software Developer",
-            "Technology Analyst"
-          ]}
-        />
-      </div>
-      <div className="row ">
-        {filteredJobs.length > 0
-          ? filteredJobs.map((job, index) => (
-              <div
-                className="card"
-                style={{
-                  width: "18rem",
-                  paddingRight: 10,
-                  paddingLeft: 10,
-                  margin: 10
-                }}
-              >
-                <img
-                  src={googleLogo}
-                  className="card-img-top"
-                  alt="..."
-                  height="150"
-                  width="150"
+        const filtered = [];
+        clearFilter();
+
+        if (type === "Type") {
+            jobs.map(job => {
+                // console.log("#2--", job);
+                if (job.typeofposition === value) {
+                    filtered.push(job);
+                }
+            });
+        }
+
+        setfilteredJobs(filtered);
+    };
+
+    const showJob = index => {
+        setJobIndex(index);
+        setModalShow(true);
+    };
+
+    return (
+        <div className={classes.ViewJobs}>
+            <VerticalModal
+                job={jobs[jobIndex]}
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
+            <div style={{ flexDirection: "row" }}>
+                <FilterButtons
+                    filter={filter}
+                    dropdownOpen={typeDropdownOpen}
+                    toggle={() => typeToggle()}
+                    filterType="Type"
+                    filterItems={["Internship", "Full Time", "Contractor"]}
                 />
-                <div
-                  style={{ padding: 0, paddingBottom: 10, marginLeft: 20 }}
-                  class="card-body"
-                >
-                  <h5 class="card-title">
-                    <div>{job.title}</div>
-                  </h5>
-                  <p class="card-text">
-                    <div>{job.typeofposition}</div>
-                    <div>{job.primaryrole}</div>
-                    <div>
-                      {job.city} - {job.state}
-                    </div>
-                    <div style={{ fontStyle: "italic", fontSize: 12 }}>
-                      {job.website}
-                    </div>
-                  </p>
-                  <button
-                    onClick={() => showJob(index)}
-                    class="btn btn-primary"
-                  >
-                    View More
-                  </button>
-                </div>
-              </div>
-            ))
-          : null}
-      </div>
-    </div>
-  );
+                <FilterButtons
+                    filter={filter}
+                    dropdownOpen={locationDropdownOpen}
+                    toggle={() => locationToggle()}
+                    filterType="Location"
+                    filterItems={["New York", "Los Angeles", "San Francisco"]}
+                />
+                <FilterButtons
+                    filter={filter}
+                    dropdownOpen={roleDropdownOpen}
+                    toggle={() => roleToggle()}
+                    filterType="Primary Role"
+                    filterItems={[
+                        "Software Engineer",
+                        "Software Developer",
+                        "Technology Analyst"
+                    ]}
+                />
+            </div>
+            <div className="row ">
+                {filteredJobs.length > 0
+                    ? filteredJobs.map((job, index) => (
+                          <div
+                              className="card"
+                              style={{
+                                  width: "18rem",
+                                  paddingRight: 10,
+                                  paddingLeft: 10,
+                                  margin: 10
+                              }}
+                          >
+                              <img
+                                  src={googleLogo}
+                                  className="card-img-top"
+                                  alt="..."
+                                  height="150"
+                                  width="150"
+                              />
+                              <div
+                                  style={{
+                                      padding: 0,
+                                      paddingBottom: 10,
+                                      marginLeft: 20
+                                  }}
+                                  class="card-body"
+                              >
+                                  <h5 class="card-title">
+                                      <div>{job.title}</div>
+                                  </h5>
+                                  <p class="card-text">
+                                      <div>{job.typeofposition}</div>
+                                      <div>{job.primaryrole}</div>
+                                      <div>
+                                          {job.city} - {job.state}
+                                      </div>
+                                      <div
+                                          style={{
+                                              fontStyle: "italic",
+                                              fontSize: 12
+                                          }}
+                                      >
+                                          {job.website}
+                                      </div>
+                                  </p>
+                                  <button
+                                      onClick={() => showJob(index)}
+                                      class="btn btn-primary"
+                                  >
+                                      View More
+                                  </button>
+                              </div>
+                          </div>
+                      ))
+                    : null}
+            </div>
+        </div>
+    );
 };
 
 export default ViewJobs;
